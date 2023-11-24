87846a7a5e0429ca016cb61bc8fe671f16d0441f
dam2aadd <dam2aadd@iescanaveral.canaveral.mostoles.ies>
2023-11-24 20:38:45 +0100

    Primer cambio en el proyecto

---
+++ src/es/iescanaveral/daw1/ed/dcc/Clase1ED.java
@@ -0,0 +1,12 @@
+package es.iescanaveral.daw1.ed.dcc;
+
+public class Clase1ED {
+	
+public static void main (String[]args) {
+	
+	Clase2ED clase2 = new Clase2ED();
+	clase2.saludar("DCC");
+
+	
+	}
+}
---
+++ src/es/iescanaveral/daw1/ed/dcc/Clase2ED.java
@@ -0,0 +1,16 @@
+package es.iescanaveral.daw1.ed.dcc;
+
+public class Clase2ED {
+	
+	void saludar(String cadena) {
+		
+		String textoSaludo=
+				"Hola soy "
+				+ cadena
+				+ " imprimiendo un mensaje editado con eclipse";
+		
+		System.out.println(textoSaludo);
+		
+	}
+
+}
